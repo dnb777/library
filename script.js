1d0d078d81e2a827445b090b471f5ffde2760c25
Damian Bruno <damianbruno17@gmail.com>
2025-02-24 11:09:10 -0300

    feat: add function "displayBooks" - creates the html elements for the cards - append the card to book-container - function "addBookToLibrary" calls "displayBooks" to add new book

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const booksContainer = document.querySelector(".books-container");
+
 const myLibrary = [];
 
 function Book(title, author, pages, status) {
@@ -10,4 +12,43 @@ function Book(title, author, pages, status) {
 function addBookToLibrary(title, author, pages, status) {
     const newBook = new Book(title,author,pages,status);
     myLibrary.push(newBook);
+    displayBooks();
 }
+
+
+// creates the html element for the card and append it to the book-container
+function displayBooks() {
+    booksContainer.innerHTML = "";
+    myLibrary.forEach(book => {
+        const card = document.createElement("div");
+        card.classList.add("card")
+    
+        const title = document.createElement("h3");
+        title.textContent = book.title;
+        title.classList.add("book-title")
+    
+        const description = document.createElement("ul");
+    
+        const author = document.createElement("li");
+        author.textContent = `Author: ${book.author}`;
+        author.classList.add("book-description");
+    
+        const pages = document.createElement("li");
+        pages.textContent = `Pages: ${book.pages}`;
+        pages.classList.add("book-description");
+    
+        const status = document.createElement("li");
+        status.textContent = `Status: ${book.status}`;
+        status.classList.add("book-description");
+    
+        
+        booksContainer.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(description);
+        description.appendChild(author);
+        description.appendChild(pages);
+        description.appendChild(status);
+    });
+}
+
+displayBooks();
