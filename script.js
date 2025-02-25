8073c5d38a6fa3f2b2d35eb394423cd1d029becd
Damian Bruno <damianbruno17@gmail.com>
2025-02-24 21:09:25 -0300

    feat: add delete button for every book card

--- script.js
+++ script.js
@@ -20,7 +20,8 @@ function addBookToLibrary(title, author, pages, status) {
 // creates the html element for the card and append it to the book-container
 function displayBooks() {
     booksContainer.innerHTML = "";
-    myLibrary.forEach(book => {
+    myLibrary.forEach((book, index) => {
+        // creates the html elements of the cards
         const card = document.createElement("div");
         card.classList.add("card")
     
@@ -41,14 +42,29 @@ function displayBooks() {
         const status = document.createElement("li");
         status.textContent = `Status: ${book.status}`;
         status.classList.add("book-description");
-    
         
+        // Add delete button with an svg
+        const deleteBtn = document.createElement("button");
+        //the data-index is used to delete the books
+        deleteBtn.setAttribute('data-index', index); 
+        deleteBtn.classList.add("card-buttons");
+        deleteBtn.id = "delete";
+        deleteBtn.innerHTML = `<svg id="delete-book" xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`;
+
+        
+        deleteBtn.addEventListener('click', (e) => {
+            const index = e.target.closest("button").getAttribute("data-index");
+            deleteBook(index);
+        })
+
+        // append's the created elements to booksContainer
         booksContainer.appendChild(card);
         card.appendChild(title);
         card.appendChild(description);
         description.appendChild(author);
         description.appendChild(pages);
         description.appendChild(status);
+        card.appendChild(deleteBtn);
     });
 }
 
@@ -69,8 +85,6 @@ newBookForm.addEventListener('submit', (e) => {
         }
     }
     const status = selected;
-
-
 
     addBookToLibrary(title, author, pages, status);
     newBookForm.reset();
